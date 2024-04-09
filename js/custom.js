abcd22d262f67f729558698e2f6d140e7fe10a60
GauravLonkar2103 <gauravlonkar1303@gmail.com>
2024-04-09 18:42:03 +0530

    Navbar done

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,32 @@ jQuery(document).ready(function() {
 	
 	"use strict";
 	// Your custom js code goes here.
+	gsap.from("#navbar-collapse ul li",{
+		duration:1,
+		y:25,
+		stagger:0.2,
+		opacity:0
 
+	})
+	gsap.from(".navbar-brand img",{
+		duration:1,
+		y:25,
+		stagger:0.2,
+		opacity:0
+
+	})
+	gsap.to(".topnav",{
+		// backgroundColor:"#E4E4E4",
+		duration:0.5,
+		border:"1px solid black",
+		scrollTrigger:{
+			trigger:"#nav",
+			scroller:"body",
+			// markers:true,
+			start:"top -10%",
+			end:"top -20%",
+			scrub:1
+	
+		}
+	})
 	})
